c29646e38354b971f09b89649152c2f57c9efdfa
Juan Wagner <juanswagner@gmail.com>
2020-11-28 11:52:29 -0300

    Require dotenv in index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use('/buildings', require('./src/controllers/buildings'));
-app.use('/customers', require('./src/controllers/customers'));
-app.use('/technicians', require('./src/controllers/technicians'));
-app.use('/appointments', require('./src/controllers/appointments'));
-app.use('/boilers', require('./src/controllers/boilers'));
+app.use("/buildings", require("./src/controllers/buildings"));
+app.use("/customers", require("./src/controllers/customers"));
+app.use("/technicians", require("./src/controllers/technicians"));
+app.use("/appointments", require("./src/controllers/appointments"));
+app.use("/boilers", require("./src/controllers/boilers"));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
