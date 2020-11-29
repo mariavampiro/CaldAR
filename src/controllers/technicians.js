379989782109adf38e9686500aedf4b5b191b2b4
Juan Wagner <juanswagner@gmail.com>
2020-11-29 14:02:58 -0300

    Update technician controllers

--- src/controllers/technicians.js
+++ src/controllers/technicians.js
@@ -1,40 +1,156 @@
-const fs = require("fs");
-const express = require("express");
-const router = express.Router();
-const technicians = require("../data/technicians.json");
+const Technicians = require("../models").technicians;
 
-router.get("/", (req, res) => {
-  const type = +req.query.type;
-  if (!type) return res.json(technicians);
+exports.findAll = (req, res) => {
+  const type = +req.query.type || "";
+  console.log(type);
 
-  return res.json(technicians.filter((t) => t.type_ids.includes(type)));
-});
+  if (!type)
+    return Technicians.find({})
+      .then((data) => res.send(data))
+      .catch((err) =>
+        res.status(500).send({ message: err.message || "Error in query db" })
+      );
 
-router.get("/:id", (req, res) => {
-  const id = +req.params.id;
-  const technician = technicians.find((t) => t.id === id);
+  return Technicians.find({ type_ids: type })
+    .then((data) => res.send(data))
+    .catch((err) =>
+      res.status(500).send({ message: err.message || "Error in query db" })
+    );
+};
 
-  if (!technician) return res.sendStatus(404);
+exports.create = (req, res) => {
+  const {
+    id,
+    first_name,
+    last_name,
+    email,
+    type_ids,
+    skills_id,
+    hour_rate,
+    daily_capacity,
+  } = req.body;
 
-  return res.json(technician);
-});
+  if (
+    !id ||
+    !first_name ||
+    !last_name ||
+    !email ||
+    !type_ids ||
+    !skills_id ||
+    !hour_rate ||
+    !daily_capacity
+  )
+    return res.status(400).send({
+      message:
+        "Uncomplete id, first_name, last_name, email, type_ids, skills_id, hour_rate, daily_capacity",
+    });
 
-router.delete("/:id", (req, res) => {
-  const id = +req.params.id;
-  const deletedTechnician = technicians.find((t) => t.id === id);
-
-  if (!deletedTechnician) return res.sendStatus(404);
-
-  const filteredTechnicians = technicians.filter(
-    (t) => t.id !== deletedTechnician.id
-  );
-
-  const techniciansJSON = JSON.stringify(filteredTechnicians);
-  fs.writeFile("./src/data/technicians.json", techniciansJSON, (err) => {
-    if (err) return res.status(500).send("Error in saving changes.");
-
-    return res.sendStatus(204);
+  const technicians = new Technicians({
+    id,
+    first_name,
+    last_name,
+    email,
+    type_ids,
+    skills_id,
+    hour_rate,
+    daily_capacity,
   });
-});
 
-module.exports = router;
+  technicians
+    .save(technicians)
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error in saving resource in DB.",
+      });
+    });
+};
+
+exports.findById = (req, res) => {
+  const id = +req.params.id || "";
+
+  Technicians.findOne({ id })
+    .then((data) => {
+      if (!data)
+        return res
+          .status(404)
+          .send({ message: `Appouintment ${req.params.id} was not found.` });
+
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error in saving resource in DB.",
+      });
+    });
+};
+
+exports.editById = (req, res) => {
+  const idTechnician = +req.params.id;
+
+  if (!idTechnician)
+    return res.status(400).send({ message: `Invalid Id ${req.params.id}` });
+
+  req.body = req.body || {};
+  const {
+    id,
+    first_name,
+    last_name,
+    email,
+    type_ids,
+    skills_id,
+    hour_rate,
+    daily_capacity,
+  } = req.body;
+
+  if (
+    !id ||
+    !first_name ||
+    !last_name ||
+    !email ||
+    !type_ids ||
+    !skills_id ||
+    !hour_rate ||
+    !daily_capacity
+  )
+    return res.status(400).send({
+      message:
+        "Uncomplete id, first_name, last_name, email, type_ids, skills_id, hour_rate, daily_capacity",
+    });
+
+  Technicians.findOneAndUpdate({ id: idTechnician }, req.body, {
+    useFindAndModify: false,
+  })
+    .then((data) => {
+      if (!data)
+        return res
+          .status(404)
+          .send({ message: `Appouintment ${req.params.id} was not found.` });
+
+      res.send({ message: `Appouintment ${req.params.id} was updated.` });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error in saving resource in DB.",
+      });
+    });
+};
+
+exports.deleteById = (req, res) => {
+  const id = +req.params.id;
+
+  if (!id)
+    return res.status(400).send({ message: `Invalid technicians Id ${id}` });
+
+  Technicians.findOneAndDelete({ id }, { useFindAndModify: false })
+    .then((data) => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error in saving resource in DB.",
+      });
+    });
+};
