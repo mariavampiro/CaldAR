a2830913d2404ef8ead82b07eac8c8460bdcadcc
Juan Wagner <juanswagner@gmail.com>
2020-12-23 19:16:21 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./src/routes');
 const db = require('./src/models');
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
